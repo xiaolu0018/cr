294e3b58a268469b6c5fba09100f619d5bfeeffa
中讯-李晓路 <lixl1@dcits.com>
2022-10-24 13:52:30 +0800

    init

---
+++ custom.js
@@ -0,0 +1,6 @@
+console.log('========= this is my plugin ======');
+document.addEventListener('DOMContentLoaded',function() {
+  var sts = document.createElement('style');
+  sts.innerHTML = ".footer.no_mod,.o3j99.c93Gbe,.s-bottom-layer.s-isindex-wrap{display:none !important;}";
+  document.head.appendChild(sts);
+})
