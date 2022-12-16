43d95ab6ce30a35fb2cb089ff50224b9f2333b14
中讯-李晓路 <lixl1@dcits.com>
2022-12-16 14:35:01 +0800

    fix(url): 适配百度地址

--- custom.js
+++ custom.js
@@ -1,6 +1,7 @@
 console.log('========= this is my plugin ======');
 document.addEventListener('DOMContentLoaded',function() {
+  const hideDomStyle = ".footer.no_mod,.o3j99.c93Gbe,#bottom_layer,.s-bottom-layer.s-isindex-wrap{display:none !important;} body.big-event-gray div{filter:none !important;}";
   var sts = document.createElement('style');
-  sts.innerHTML = ".footer.no_mod,.o3j99.c93Gbe,.s-bottom-layer.s-isindex-wrap{display:none !important;}";
+  sts.innerHTML = hideDomStyle;
   document.head.appendChild(sts);
 })
